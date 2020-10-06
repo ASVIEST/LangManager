04fb889ef094fba288c6740b60d6e3b3b8172e3e
ASVIEST <71895914+ASVIEST@users.noreply.github.com>
2020-10-06 13:45:20 +0300

    Add french, kannada, Japanese, polish, german, Italian language support

--- langmanager.py
+++ langmanager.py
@@ -12,6 +12,12 @@ lan = ''
 enpath = 'en.txt'
 rupath = 'ru.txt'
 zhpath = 'zh.txt'
+frpath = 'fr.txt'
+knpath = 'kn.txt'
+japath = 'ja.txt'
+plpath = 'pl.txt'
+depath = 'de.txt'
+itpath = 'it.txt'
 
 #select language
 def lang(language='en'):
@@ -49,6 +55,35 @@ def zh_translate(path):
     translate_zh = format_to_pydictionary(path)
     translate_zh = ast.literal_eval(translate_zh)
 
+def fr_translate(path):
+    global translate_fr
+    translate_fr = format_to_pydictionary(path)
+    translate_fr = ast.literal_eval(translate_fr)
+
+def kn_translate(path):
+    global translate_kn
+    translate_kn = format_to_pydictionary(path)
+    translate_kn = ast.literal_eval(translate_kn)
+
+def ja_translate(path):
+    global translate_ja
+    translate_ja = format_to_pydictionary(path)
+    translate_ja = ast.literal_eval(translate_ja)
+
+def pl_translate(path):
+    global translate_pl
+    translate_pl = format_to_pydictionary(path)
+    translate_pl = ast.literal_eval(translate_pl)
+
+def de_translate(path):
+    global translate_de
+    translate_de = format_to_pydictionary(path)
+    translate_de = ast.literal_eval(translate_de)
+
+def it_translate(path):
+    global translate_it
+    translate_it = format_to_pydictionary(path)
+    translate_it = ast.literal_eval(translate_it)
 
 #change path
 def filepath_en(path):
@@ -63,6 +98,29 @@ def filepath_zh(path):
     global zhpath
     zhpath = path
 
+def filepath_fr(path):
+    global frpath
+    frpath = path
+
+def filepath_kn(path):
+    global knpath
+    knpath = path
+
+def filepath_ja(path):
+    global knpath
+    knpath = path
+
+def filepath_pl(path):
+    global plpath
+    plpath = path
+
+def filepath_de(path):
+    global depath
+    depath = path
+
+def filepath_it(path):
+    global itpath
+    itpath = path
 
 def translate_get(text=''):
     if lan == 'english':
@@ -73,7 +131,31 @@ def translate_get(text=''):
         ru_translate(rupath)
         translate = translate_ru[text]
         return translate
-    elif lan == 'chinese':
+    if lan == 'chinese':
         zh_translate(zhpath)
         translate = translate_zh[text]
+        return translate
+    if lan == 'french':
+        fr_translate(frpath)
+        translate = translate_fr[text]
+        return translate
+    if lan == 'kannada':
+        kn_translate(knpath)
+        translate = translate_kn[text]
+        return translate
+    if lan == 'japanese':
+        ja_translate(knpath)
+        translate = translate_ja[text]
+        return translate
+    if lan == 'polish':
+        pl_translate(plpath)
+        translate = translate_pl[text]
+        return translate
+    if lan == 'german':
+        de_translate(depath)
+        translate = translate_de[text]
+        return translate
+    elif lan == 'italian':
+        it_translate(itpath)
+        translate = translate_it[text]
         return translate
